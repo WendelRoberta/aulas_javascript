b177325697890639d746adf1f8d8b97bb39d99f3
Wendel Roberta <wendel.roberta1995@gmail.com>
2020-10-03 19:00:08 -0300

    Finalizando introdução a js, introdução a functions

--- script.js
+++ script.js
@@ -2,6 +2,30 @@ function clicou() {
     document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
 }
 
+function redirecionamento() {
+    window.open("https://github.com/WendelRoberta/aulas_javascript") //abre em outra aba
+        //window.location.href = "https://github.com/WendelRoberta/aulas_javascript" //abre na mesma aba
+}
+
+function trocar(elemento) {
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar aqui");
+}
+
+function voltar(elemento) {
+    elemento.innerHTML = "Passe o mouse aqui";
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+function carrega() {
+    alert("pagina carregada");
+}
+
+function escolha(elemento) {
+    console.log(elemento.value);
+}
+
 
 
 
